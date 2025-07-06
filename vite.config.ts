a3ffb1efe3eac8ce0e7c4b8299164610842bdfdd
Saranya Sidharth <saranyasidhartht@gmail.com>
2025-07-06 16:11:56 +0530

    chore: add svg.d.ts for importing SVGs as React components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
+import svgr from "vite-plugin-svgr";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+});
